09b6ccf4aaadda9638f3772bfcbe0b3476181386
Chamindu Sauranga <chamindu1567@gmail.com>
2023-05-13 10:57:12 +0530

    deletion

--- src/main/scala/storeDataIssue/constants.scala
+++ src/main/scala/storeDataIssue/constants.scala
@@ -4,7 +4,7 @@ object constants {
     val prfAddrWidth    =   6
     val branchMaskWidth =   4
 
-    val fifo_width      = 1.U + prfAddrWidth + branchMaskWidth + 1.U
+    val fifo_width      = 1 + prfAddrWidth + branchMaskWidth + 1
     val fifo_depth      = 16 
 }
 
